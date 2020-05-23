072255f2718a114a488b2c6df3628d3d8007e18c
Coskun Uyar <coskunuyarr@gmail.com>
2020-05-24 01:32:32 +0300

    Clear completed feature is added.

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,30 +1,32 @@
 <template>
   <div>
       <input type="text" class="todo-input" placeholder="what needs to be done?" v-model="newTodo" @keyup.enter="addTodo" />
-      <div v-for="(todo,index) in todosFiltered" :key="todo.id" class="todo-item">
-          <div class="todo-item-left">
-              <input type="checkbox" v-model="todo.completed"/>
-              <div 
-                v-if="!todo.editing" 
-                @dblclick="editTodo(todo)" 
-                class="todo-item-label" :class="{ completed : todo.completed }">
-                {{todo.title}}
-              </div>
-              <input 
-                v-else 
-                class="todo-item-edit" 
-                type="text" 
-                v-model="todo.title" 
-                @blur="doneEdit(todo)"
-                @keyup.enter="doneEdit(todo)"
-                @keyup.esc="cancelEdit(todo)"
-                v-focus
-                />
-          </div>
-          <div class="remove-item" @click="removeTodo(index)">
-              &times;
-          </div>
-      </div>
+      <transition-group name="face" enter-active-class="animated fadeInUp" leave-active-class="animated fadeOutDown">
+        <div v-for="(todo,index) in todosFiltered" :key="todo.id" class="todo-item">
+            <div class="todo-item-left">
+                <input type="checkbox" v-model="todo.completed"/>
+                <div 
+                    v-if="!todo.editing" 
+                    @dblclick="editTodo(todo)" 
+                    class="todo-item-label" :class="{ completed : todo.completed }">
+                    {{todo.title}}
+                </div>
+                <input 
+                    v-else 
+                    class="todo-item-edit" 
+                    type="text" 
+                    v-model="todo.title" 
+                    @blur="doneEdit(todo)"
+                    @keyup.enter="doneEdit(todo)"
+                    @keyup.esc="cancelEdit(todo)"
+                    v-focus
+                    />
+            </div>
+            <div class="remove-item" @click="removeTodo(index)">
+                &times;
+            </div>
+        </div>
+      </transition-group>
       <div class="extra-container">
             <div><label><input 
             type="checkbox" 
@@ -40,7 +42,9 @@
               <button :class="{active: filter === 'completed'}" @click="filter = 'completed'">Completed</button>
           </div>
           <div>
-              Clear Completed
+              <transition fade>
+                    <button v-if="showClearCompletedButton" @click="clearCompleted">Clear Completed</button>
+              </transition>
           </div>
       </div>
   </div>
@@ -86,6 +90,9 @@ export default {
           }else if(this.filter === "completed"){
               return this.todos.filter(todo => todo.completed); 
           }
+      },
+      showClearCompletedButton(){
+          return this.todos.filter(todo => todo.completed).length > 0;
       }
   },
   directives:{
@@ -127,12 +134,16 @@ export default {
       },
       checkAllTodos(){
           this.todos.forEach(todo => todo.completed = event.target.checked);
+      },
+      clearCompleted(){
+          this.todos = this.todos.filter(todo => !todo.completed);
       }
   }
 }
 </script>
 
 <style lang="scss">
+    @import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.compat.min.css");
     .todo-input{
         width: 100%;
         padding: 10px 18px;
@@ -148,6 +159,7 @@ export default {
         display: flex;
         align-items: center;
         justify-content: space-between;
+        animation-duration: 0.3s;
     }
 
     .remove-item{
@@ -201,7 +213,8 @@ export default {
         font-size: 14px;
         background-color: white;
         appearance: none;
-
+        border: 1px solid grey;
+        padding: 10px;
         &:hover{
             background: lightgreen;
         }
@@ -212,5 +225,13 @@ export default {
     }
     .active{
         background: lightgreen;
+    }
+
+    .fade-enter-active, .fade-leave-active{
+        transition: opacity .2s;
+    }
+
+    .fade-enter, .fade-leave-to{
+        opacity: 0;
     }
 </style>
